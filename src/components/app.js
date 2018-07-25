2c8c5dbf1d1ae95c8da0e0e80a2ef413cdbfefef
Marc Freiman <marcfb2@hotmail.com>
2018-07-25 13:43:01 -0400

    Commit React excercise

---
+++ src/components/app.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class App extends React.Component{
+  render(){
+    return(
+      <ul>
+      <li> Marc</li>
+      <li> Alan</li>
+      <li> Alexander</li>
+      </ul>
+    )
+  }
+}
+
+export default App;
